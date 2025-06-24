525d47b56d2f038b08b7456292526a8ffa992344
오건근 <ogeongeun@example.com>
2025-06-25 04:32:27 +0900

    백엔드 기능 삭제

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from 'react';
+
 import { Link } from 'react-router-dom';
 
 const Home = () => {
-  const [count, setCount] = useState(0);
+  
 
   
   const features = [
